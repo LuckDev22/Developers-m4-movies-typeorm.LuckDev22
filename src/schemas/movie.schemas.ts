ceb8b1cb337f469a2cad75488e7c27510a801aa6
LuckDev22 <103015314+LuckDev22@users.noreply.github.com>
2023-05-02 19:37:04 -0300

    feat: all test created passed

--- src/schemas/movie.schemas.ts
+++ src/schemas/movie.schemas.ts
@@ -4,8 +4,8 @@ export const movieSchema = z.object({
     id: z.number(),
     name: z.string().max(50),
     description: z.string().nullish(),
-    duration: z.number(),
-    price: z.number(),
+    duration: z.number().min(0, {message: "Number must be greater than 0"}),
+    price: z.number().min(0).int(),
 });
 
 export const movieSchemaReq = movieSchema.omit({ id: true });
@@ -18,5 +18,5 @@ export const allMovieSchemaResp = z.object({
     prevPage: z.string().nullable(),
     nextPage: z.string().nullable(),
     count: z.number(),
-    data: movieSchemaResp
-})
+    data: movieSchemaResp,
+});
